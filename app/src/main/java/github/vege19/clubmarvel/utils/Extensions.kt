7bee8c1b09c7188d0334215c7cea8e04b32349d8
Josue <josue.ardon113@gmail.com>
2019-07-26 17:14:55 -0600

    Progressbar while image is loading. | Error image.

--- app/src/main/java/github/vege19/clubmarvel/utils/Extensions.kt
+++ app/src/main/java/github/vege19/clubmarvel/utils/Extensions.kt
@@ -3,6 +3,7 @@ package github.vege19.clubmarvel.utils
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.View
@@ -13,8 +14,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.GenericTransitionOptions
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
 import com.ethanhua.skeleton.Skeleton
 import github.vege19.clubmarvel.App
 import github.vege19.clubmarvel.R
@@ -49,16 +52,26 @@ fun ViewModel.makeRetrofitCallback(action: suspend () -> Unit) {
 
 //Set image with glide
 fun ImageView.setGlideImage(url: String, context: Context, hasZoomAnimation: Boolean, width: Int?, height: Int?) {
+    val progressDrawable = CircularProgressDrawable(App.getContext())
+    progressDrawable.setColorSchemeColors(App.getContext().resources.getColor(R.color.colorAccent))
+    progressDrawable.centerRadius = 30f
+    progressDrawable.strokeWidth = 5f
+    progressDrawable.start()
+
     if (hasZoomAnimation) {
         Glide.with(context)
                 .load(url)
+                .placeholder(progressDrawable)
                 .transition(GenericTransitionOptions.with(R.anim.anim_zoom_out))
+                .error(App.getContext().getDrawable(R.drawable.ic_close_24dp))
                 .centerCrop()
                 .override(width?:0, height?:0)
                 .into(this)
     } else {
         Glide.with(context)
                 .load(url)
+                .placeholder(progressDrawable)
+                .error(App.getContext().getDrawable(R.drawable.ic_vector_not_found))
                 .centerCrop()
                 .override(width!!, height!!)
                 .into(this)
